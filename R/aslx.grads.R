f0c8d6482ddea0f4062124aa17b7f3f15e4077f6
Michail Tsagris <mtsagris@yahoo.gr>
2025-11-12 13:16:05 +0200

    change

--- R/aslx.grads.R
+++ R/aslx.grads.R
@@ -5,10 +5,10 @@ aslx.grads <- function(y, x, a, be, gama, coords, k = 10) {
   nam <- colnames(x)
   if ( is.null( nam ) ) {
     p <- NCOL(x) 
-    nam <- paste("WX", 1:p, sep = "") 
-  }
-  colnames(Wx) <- nam
+    nam <- c( paste("X", 1:p, sep = ""), paste("WX", 1:p, sep = "") ) 
+  } else  nam <- c(nam, paste("W", nam, sep = "") )
   X <- cbind(x, Wx)
+  colnames(X) <- nam
   be <- rbind(be, gama)
   CompositionalSR::ar.grads(y, X, a, be)
 }
